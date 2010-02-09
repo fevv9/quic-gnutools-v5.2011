9f73079334e145a63d73ae5d03b394cff9bcc71d
emenezes <>
2010-02-09 20:11:59 +0000

    Back down accidental check in.

--- bfd/version.h
+++ bfd/version.h
@@ -1,8 +1,7 @@
-#define BFD_VERSION_DATE : Hexagon Build Version () 
+#define BFD_VERSION_DATE : Hexagon Build Version X.Y.ZZ XXXXXXXXXXX_YYYY_ZZZZZ
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
-#define QC_COPYRT_MSG "Copyright 2005-2010 Qualcomm, Inc.   All Rights Reserved"
 #ifndef QC_COPYRT_MSG
 #define QC_COPYRT_MSG "Copyright YYYY-YYYY Qualcomm, Inc.  All Rights Reserved."
 #endif
