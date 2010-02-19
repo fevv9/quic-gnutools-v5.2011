fb4ae4c69d3a70c6fc2f73a2dc229fef0661c418
emenezes <>
2010-02-19 22:20:14 +0000

    Restore original version patterns.

--- bfd/version.h
+++ bfd/version.h
@@ -1,10 +1,7 @@
-#define BFD_VERSION_DATE : Hexagon Build Version (UNKNOWN) BT_201002152119_emenezes
+#define BFD_VERSION_DATE : Hexagon Build Version X.Y.ZZ XXXXXXXXXXX_YYYY_ZZZZZ
 #define BFD_VERSION @bfd_version@
 #define BFD_VERSION_STRING  @bfd_version_package@ @bfd_version_string@
 #define REPORT_BUGS_TO @report_bugs_to@
-#define QC_COPYRT_MSG "Copyright 2005-2010 Qualcomm, Inc.   All Rights Reserved"
-#define QC_COPYRT_MSG "Copyright 2005-2010 Qualcomm, Inc.   All Rights Reserved"
-#define QC_COPYRT_MSG "Copyright 2005-2010 Qualcomm, Inc.   All Rights Reserved"
 #ifndef QC_COPYRT_MSG
 #define QC_COPYRT_MSG "Copyright YYYY-YYYY Qualcomm, Inc.  All Rights Reserved."
 #endif
