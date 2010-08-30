577dbc7de68ff93d55b786a8e8145957cdc9f7e1
emenezes <>
2010-08-30 15:00:42 +0000

    Allow the page-size to be changed via the command-line.

--- ld/emulparams/qdsp6elf.sh
+++ ld/emulparams/qdsp6elf.sh
@@ -6,15 +6,13 @@ EXTRA_EM_FILE=qdsp6elf
 OUTPUT_FORMAT="elf32-littleqdsp6"
 LITTLE_OUTPUT_FORMAT="elf32-littleqdsp6"
 BIG_OUTPUT_FORMAT="elf32-bigqdsp6"
-# EMBEDDED should be yanked for a real OS, e.g., Linux.
-EMBEDDED=" "
-MAXPAGESIZE=4096
-# TEXT_START_ADDR should be 0 for embedded.
-TEXT_START_ADDR=${EMBEDDED+0}${EMBEDDED-${MAXPAGESIZE}}
+EMBEDDED=yes
+MAXPAGESIZE="CONSTANT (MAXPAGESIZE)"
+TEXT_START_ADDR=0
 NONPAGED_TEXT_START_ADDR=0
 ENTRY=start
-TEXT_DYNAMIC=" "
-DATA_GOT=" "
+DATA_GOT=yes
+SEPARATE_GOTPLT=0
 GENERATE_SHLIB_SCRIPT=yes
 GENERATE_COMBRELOC_SCRIPT=yes
 # QDSP6 V2 NOP
@@ -23,8 +21,8 @@ NOP=0x00c0007f
 ALIGNMENT=64 # L2
 OTHER_EXCLUDE_FILES=fini.o
 SBSS_START_SYMBOLS="PROVIDE (${USER_LABEL_PREFIX}__sbss_start = .);
-                    PROVIDE (${USER_LABEL_PREFIX}___sbss_start = .);"
+    PROVIDE (${USER_LABEL_PREFIX}___sbss_start = .);"
 SBSS_END_SYMBOLS="PROVIDE (${USER_LABEL_PREFIX}__sbss_end = .);
-                  PROVIDE (${USER_LABEL_PREFIX}___sbss_end = .);"
+    PROVIDE (${USER_LABEL_PREFIX}___sbss_end = .);"
 SDATA_START_SYMBOLS="__default_sda_base__ = .;
-                     PROVIDE (_SDA_BASE_ = .);"
+    PROVIDE (_SDA_BASE_ = .);"
